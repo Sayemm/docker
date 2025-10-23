653bcabd77c73646176aebccff6f295319229bdc
Sayemm <sayemseer@gmail.com>
2025-10-23 13:46:43 -0600

    Docker - platform which load/unload container

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+}
+
+/*
+What is Docker?
+---------------
+- Old days loading to the ship was hectic (barrels, boxes were used)
+- Then container comes into the play - storing/load/unload.. become earlier
+
+- Dock - (Ferighat)
+- Docker - Person who loads unloads container at dock / Longshoreman / Stevedore
+- Docker - Container loads/unloads to the ship (computer)
+- Container (OS, Software, dependency....)
+- Image - Container screenshot
+- Can give/distribute this image to another one
+	- run using docker in another PC
+	- That imgae will create another identical container
+
+=> Docker is a platform
+
+	PC1						 PC2
+Python - 1.2			Python - 3.1
+- Application			- Not working here
+
+=> Solomon Hykes - dotcloud - docker inc
+- Created internal tool - Docker
+
+
+Youtube: https://www.youtube.com/watch?v=I62nYMQyz48&list=PLpCqPSEm2Xe8dVi8cCLM9jmRp-FtEIGil&index=5
+*/
