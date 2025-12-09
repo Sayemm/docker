74277fd1da20c21cf5dd5b1e3062cb77dfe13713
Sayemm <sayemseer@gmail.com>
2025-12-09 12:12:14 -0600

    Linux Basic Commands & Linux File System

--- main.go
+++ main.go
@@ -7,52 +7,66 @@ func main() {
 }
 
 /*
-LINUX PACKAGES
---------------
-- complete OS already has a package manager which can install/uninstall packages (vlc, git) those are within a repository
-	- high level - apt
-		=> to install a package it might need something beforehand in the system
-		=> apt will find those dependencies first and install those with dpkg first before install the main package
-		=> can upgrade (new version)
+Linux Basic Commands
+--------------------
+- sayem@Mac ~ %
+	- user name is sayem who logged in to the machine Mac
+	- ~ -> home directory
+	- % -> general user (% also meaning z shell, $ means bash shell, # means super user)
 
-	- low level - dpkg
-		=> this installs packages in the end => apt talks with dpkg to install the packages
-		=> if we directly try to install a package using dpkg it might fail, as it won't tell us which things it needs beforehand to install that package
+- Linux File System
+	- / -> root directory
+		- /bin -> binaries (basic commands - ls, cat..)
+		- /boot-> kernel & bootloader
+		- /dev -> devices (like software needs for wifi adapter)
+		- /etc -> configurations
+		- /home-> various user home
+		- /lib -> libraries
+		- /media> mounts (external hardisks)
+		- /proc-> kernal & process information
+		- /sbin-> system binaries
+		- /tmp -> temporary file
+		- /usr -> user programs (all applications)
+		- /var -> logs, cache
+		........................
 
-- ubuntu package manager  - (apt, apt-get = high, dpkg = low)
-- redhat, centos, fedora - (yum, dnf = high, rpm)
-- arch - packman
-- openSuse - zypper
-=> Each package manager has its own repository system
-- repository has an URL
-- package has a format (.deb for ubuntu, .rpm for yum)
+- pwd (present working directory)
+- ls
+	- ls -l  (long detail view)
+	- ls -a  (hidden files)
+	- ls -la (hiddend and long list)
 
-- apt install git
-	=> apt will search git in the repository (where everything is stored for that package manager)
-	=> apt has the URL and then apt will check whether that URL has git or not
+- cd
+	- cd / -> go to root path
+	- cd ~ -> home directory
+	- cd - -> last use immediate directory
+	- cd .. -> parent directory
 
-- package (git, vlc) includes
-	=> program (binary)
-	=> metadata
-	=> dependencies
-	=> install scripts (pre-install, post-install, pre-remove, post-remove)
-	=> config files
+- mkdir app               -> create app folder
+- mkdir -p sayem/sayed    -> folder within folder
+- touch filename (a.txt)     -> create a file
+
+- cp file1.txt file2.txt     -> create file2.txt and copy the content from file1.txt
+- cp -r sayed/ munni/        -> create another folder munni and recursively copy all the contents from sayed
+- cp file2.txt ../sayed/file3.txt -> copy file2.txt to another folder and create file3.txt
+
+=> relative path => ../sayed/file3.txt
+=> absolute path => /app/sayem/sayem => start from the root
+
+- echo hello world >> a.txt  -> append content to the file
+- echo hello world > a.txt.  -> remove old content and just add "hello world"
+- cat filenane (a.txt)       -> see the contents of the file
+- head -n 5 a.txt            -> see first 5 lines from the txt
+- tail -n 5 a.txt            -> see last 5 lines from the txt
+- rm filename (a.txt)        -> remove a file not directory
+- rm -r foldername           -> remove the folder recursively (delete one by one in that folder then remote the main folder)
+- rm -rf foldername          -> forcefully remove the folder
+
+- mv file3.txt file4.txt     -> rename file3 to file4
 
 
 
 
 
-
-================> WHY DO WE NEED PACKAGE MANAGER IN LINUX
-
-Linux applications depend on system-provided shared libraries.
-Apps like VLC, Chrome, and Firefox do not include their own audio, video, network, or GUI libraries—they rely on the same common libraries installed in the Linux system.
-Because of this, Linux needs a package manager (like apt) to automatically install and manage all these required shared libraries and keep them updated.
-
-Windows applications usually bring their own libraries inside their .exe or .msi installers.
-Each app bundles its own audio, video, GUI, and network DLLs, so they don’t rely on system-wide libraries.
-Since Windows apps include their dependencies themselves, a package manager is not required for normal software installation.
-
-
-Youtube: https://www.youtube.com/watch?v=W7uuHbEv528&list=PLpCqPSEm2Xe8dVi8cCLM9jmRp-FtEIGil&index=18
+Youtube: https://www.youtube.com/watch?v=6WL7VOX55kg&list=PLpCqPSEm2Xe8dVi8cCLM9jmRp-FtEIGil&index=19
 */
